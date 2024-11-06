87c9fd82cbc8362afedaf714105e8c982831dabe
ppzh0 <narrtor06@gmail.com>
2024-11-06 22:16:25 +0630

    move function to index.js

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,7 @@
-console.log("Wanna Add Stories Or Speeches? Contact Me On Telegram; @ppzh0")
+console.log("Wanna Add Stories Or Speeches? Contact Me On Telegram; @ppzh0");
+
+// Check if the user agent contains 'Mozilla' and is not a WebView or other browsers
+const isMozilla = navigator.userAgent.includes('Mozilla/') && !navigator.userAgent.includes('Chrome') && !navigator.userAgent.includes('Safari');
+if (isMozilla) {
+    document.getElementById('mozOpen').setAttribute('open', 'open');
+}
