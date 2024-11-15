3a9c6e45df4a50e61beae03ee8c42c4d5ee28acc
ppzh0 <narrtor06@gmail.com>
2024-11-15 08:58:25 +0630

    fix line-height and typos

--- assets/index.js
+++ assets/index.js
@@ -1,22 +1,23 @@
 console.log("Wanna Add Stories Or Speeches? Contact Me On Telegram; @ppzh0");
 
-// Check if the user agent contains 'Mozilla' and is not a WebView or other browsers
-const isMozilla = navigator.userAgent.includes('Mozilla/') && !navigator.userAgent.includes('Chrome') && !navigator.userAgent.includes('Safari');
+// Check if the user agent contains "Mozilla" and is not a WebView or other browsers
+const isMozilla = navigator.userAgent.includes("Mozilla/") && !navigator.userAgent.includes("Chrome") && !navigator.userAgent.includes("Safari");
 if (isMozilla) {
-    document.getElementById('mozOpen').setAttribute('open', 'open');
+    document.getElementById("mozOpen").setAttribute("open", "open");
 }
 
 // Change font if the checkbox is checked
-const toggleFontBtn = document.getElementById('toggleFontBtn');
+const toggleFontBtn = document.getElementById("toggleFontBtn");
 let isSerif = true;  // Track current font
 
-toggleFontBtn.addEventListener('click', function () {
+toggleFontBtn.addEventListener("click", function () {
     if (isSerif) {
-        document.body.style.fontFamily = 'Alsina, sans-serif';
-        document.body.style.fontSize = '2.6rem';
+        document.body.style.fontFamily = "'Alsina', sans-serif";
+        document.body.style.fontSize = "2.6rem";
+        document.body.style.lineHeight = "1.3";
     } else {
-        document.body.style.fontFamily = 'serif';
-        document.body.style.fontSize = '130%';
+        document.body.style.fontFamily = "serif";
+        document.body.style.fontSize = "130%";
     }
     // Toggle the font state
     isSerif = !isSerif;
